071c3f7b5a647b1f6b323c7af66281ec4413e513
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-06-18 13:17:49 -0400

    added account route and import

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
-const blogRouter = require('./src/routes/blogRouter.js')
+const blogRouter = require('./src/routes/blogRouter.js');
+const userRouter = require('./src/routes/userRouter.js');
+const accountRouter = require('./src/routes/accountRouter.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/blog', blogRouter)
+app.use('/blog', blogRouter);
+app.use('/user', userRouter);
+app.use('/account', accountRouter);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000.')
